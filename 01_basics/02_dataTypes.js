7adeae8fde2bf2150f8de9955c62cdfb81281777
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-09 10:02:32 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Harekrishna")
+
+let name = "harekrishna"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+
+//boolean - false or true
+//null -> standalone value
+//undefined -> 
+//symbol -> unique
+
+//object
+console.log(typeof "hare")
