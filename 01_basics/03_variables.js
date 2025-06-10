19c63ac08b834b724d1d70110ecb3c37c5b6a44f
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-10 05:57:49 +0000

    Conversion

---
+++ 01_basics/03_variables.js
@@ -0,0 +1,30 @@
+let score = 33
+// console.log(typeof score);
+
+let score1 = 33
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Harekrishna"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Harekrishna" => true
+
+let stringNum = 33
+let covertString = String(stringNum)
+console.log(covertString);
+console.log(typeof covertString)
