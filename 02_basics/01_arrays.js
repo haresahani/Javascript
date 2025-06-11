38c7a5e1496467b1bdcb55939eedbdd0b0921b7d
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-11 11:29:22 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const name = ["Hare", "Krishna", "Kumar"]
+
+const arr = new Array(1, 2, 3, 4)
+// console.log(name[1]);
+
+//array methods
+
+myArr.push(6)
+myArr.push(7)
+// myArr.pop()
+
+
+// myArr.unshift(9);
+// myArr.shift();
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9))
+// console.log(myArr)
+
