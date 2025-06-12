c5f3d244414bffde801f869235a69c6e75c3fdd2
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-12 11:00:35 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,4 +24,14 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// console.log(obj3);
+
+const course = {
+    coursename: "js in hindi",
+    coureprice: 999,
+    coursepinstructor: "Haresahani"
+}
+
+const {coursepinstructor} = course
+
+console.log(coursepinstructor)
