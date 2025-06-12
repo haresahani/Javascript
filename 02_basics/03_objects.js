a453f6a1df9b12b65065c2e93b5e6e17c2f3c150
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-12 08:47:26 +0000

    objects - 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+//singleton
+
+//Object literals
+
+const mySym = Symbol("Key1")
+
+const JsUser = {
+    name: "Harekrishna",
+    age: 18,
+    [mySym]: "mykey1",
+    location: "jaipur",
+    email: "hare@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser[mySym])
+
+// JsUser.email = "hare@chatgpt.com"
+// Object.freeze(JsUser)
+// JsUser.email = "hare@microsoft.com"
+// console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+console.log(JsUser.greeting());
+JsUser.greetingTwo = function(){
+    console.log(`Hellow JS user, ${this.name}`)
+}
+console.log(JsUser.greetingTwo())
