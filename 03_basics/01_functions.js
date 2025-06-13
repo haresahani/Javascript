d445f377aeb586fbb64e5fe032f42d262d7a2eb3
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-13 08:54:37 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,42 @@
+function myName() {
+    console.log("H")
+    console.log("A")
+    console.log("R")
+    console.log("E")
+}
+
+// myName()
+
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1+num2)
+// } 
+
+// addTwoNumbers(3, 5)
+// addTwoNumbers(3, "5")
+// addTwoNumbers(3, null)
+// addTwoNumbers(3)
+
+function addTwoNumbers(num1, num2) {
+    // let result = (num1+num2)
+    return num1+num2
+} 
+
+// addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username) {
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Harekrishna"))
+
+function loginUsermessage(username) {
+    if(!username) {
+        console.log("Please enter a usernam]")
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage())
