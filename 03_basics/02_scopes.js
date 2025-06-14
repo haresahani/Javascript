be31b6a76fdd97b1e57c6c702f12c0519f552bf1
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-14 11:04:10 +0000

    SCOPE

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,27 @@
+function one() {
+    const username = "Harekrishna"
+
+    function two() {
+        const website = "YOutube"
+        // console.log(username)
+    }
+
+    // two();
+}
+
+// one();
+
+if(true) {
+    const username = "harekrishna"
+    if(username === "harekrishna" ) {
+        const website = "Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
