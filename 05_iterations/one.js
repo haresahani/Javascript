4a0371d04e5d117fd3cc7d26220535cefd384103
haresahani <111163929+haresahani@users.noreply.github.com>
2025-06-16 10:38:39 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,26 @@
+// for loop
+// for(let i = 0; i<10; i++) {
+//     if(i == 5) {
+//         console.log("FOund")
+//     }
+//     console.log(i);
+// }
+
+//for of - loop
+// let arr = [2, 4, 5, 6, 7];
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+// const greetings = "Hello world";
+// for(const greet of greetings) {
+//     console.log(greet)
+// }
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+console.log(map)
+
